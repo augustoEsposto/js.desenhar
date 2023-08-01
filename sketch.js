3169f8d07cffa41040440ee50eec196e84e04631
augustoEsposto <138055731+augustoEsposto@users.noreply.github.com>
2023-08-01 18:24:12 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(940, 800); 
+    background('white');
+  }
+  
+  function draw() {
+    
+    
+    stroke ('black');
+    fill ('purple')
+    
+    //console.log (mouseIsPressed);
+    
+    if (mouseIsPressed) {
+      
+      rect (mouseX, mouseY, 40, 40);}
+    }
+    
